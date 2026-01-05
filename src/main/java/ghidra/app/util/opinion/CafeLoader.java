cedad8956f75036a721ff97d90538fcfd347a05f
Maschell <Maschell@gmx.de>
2026-01-05 12:56:09 +0100

    Fixes for Ghidra 12.0

--- src/main/java/ghidra/app/util/opinion/CafeLoader.java
+++ src/main/java/ghidra/app/util/opinion/CafeLoader.java
@@ -21,61 +21,66 @@ import ghidra.util.exception.CancelledException;
 import ghidra.util.task.TaskMonitor;
 
 public class CafeLoader extends ElfLoader {
-	@Override
-	public Collection<LoadSpec> findSupportedLoadSpecs(ByteProvider provider) throws IOException {
-		byte[] header = new byte[] { 0x7F, 0x45, 0x4C, 0x46, 0x01, 0x02, 0x01, (byte) 0xCA, (byte) 0xFE };
-		List<LoadSpec> loadSpecs = new ArrayList<>();
+    @Override
+    public Collection<LoadSpec> findSupportedLoadSpecs(ByteProvider provider) throws IOException {
+        byte[] header = new byte[]{0x7F, 0x45, 0x4C, 0x46, 0x01, 0x02, 0x01, (byte) 0xCA, (byte) 0xFE};
+        List<LoadSpec> loadSpecs = new ArrayList<>();
 
-		if (Arrays.equals(provider.readBytes(0, header.length), header)) {
-			List<QueryResult> results = QueryOpinionService.query(getName(), "wiiu", null);
-			boolean hasGekkoProcessor = false;
+        if (Arrays.equals(provider.readBytes(0, header.length), header)) {
+            List<QueryResult> results = QueryOpinionService.query(getName(), "wiiu", null);
+            boolean hasGekkoProcessor = false;
 
-			for (QueryResult result : results) {
-				if (result.pair.languageID.getIdAsString().contains("Gekko")) {
-					hasGekkoProcessor = true;
-				}
-			}
+            for (QueryResult result : results) {
+                if (result.pair.languageID.getIdAsString().contains("Gekko")) {
+                    hasGekkoProcessor = true;
+                }
+            }
 
-			for (QueryResult result : results) {
-				if (result.pair.languageID.getIdAsString().contains("Gekko")) {
-					loadSpecs.add(new LoadSpec(this, 0, new QueryResult(result.pair, true)));
-				} else {
-					loadSpecs.add(new LoadSpec(this, 0, new QueryResult(result.pair, hasGekkoProcessor ? false : result.preferred)));
-				}
-			}
+            for (QueryResult result : results) {
+                if (result.pair.languageID.getIdAsString().contains("Gekko")) {
+                    loadSpecs.add(new LoadSpec(this, 0, new QueryResult(result.pair, true)));
+                } else {
+                    loadSpecs.add(new LoadSpec(this, 0, new QueryResult(result.pair, hasGekkoProcessor ? false : result.preferred)));
+                }
+            }
 
-			if (loadSpecs.isEmpty()) {
-				loadSpecs.add(new LoadSpec(this, 0, new LanguageCompilerSpecPair("PowerPC:BE:32:default", "default"), true));
-			}
-		}
+            if (loadSpecs.isEmpty()) {
+                loadSpecs.add(new LoadSpec(this, 0, new LanguageCompilerSpecPair("PowerPC:BE:32:default", "default"), true));
+            }
+        }
 
-		return loadSpecs;
-	}
+        return loadSpecs;
+    }
 
-	@Override
-	public String getName() {
-		return "Wii U / CafeOS Binary (RPX/RPL)";
-	}
+    @Override
+    public String getName() {
+        return "Wii U / CafeOS Binary (RPX/RPL)";
+    }
 
-	@Override
-	public LoaderTier getTier() {
-		return LoaderTier.SPECIALIZED_TARGET_LOADER;
-	}
+    @Override
+    public LoaderTier getTier() {
+        return LoaderTier.SPECIALIZED_TARGET_LOADER;
+    }
 
-	@Override
-	public int getTierPriority() {
-		return 0;
-	}
+    @Override
+    public int getTierPriority() {
+        return 0;
+    }
 
-	@Override
-	public void load(ByteProvider provider, LoadSpec loadSpec, List<Option> options, Program program,
-			TaskMonitor monitor, MessageLog log) throws IOException, CancelledException {
-		try {
-			byte[] data = RplConverter.convertRpl(provider, log::appendMsg);
-			RplHeader rpl = new RplHeader(new ByteArrayProvider(data), log::appendMsg);
-			ElfProgramBuilder.loadElf(rpl, program, options, log, monitor);
-		} catch (ElfException | DataFormatException var8) {
-			throw new IOException(var8.getMessage());
-		}
-	}
+    @Override
+    public void load(Program program, Loader.ImporterSettings settings) throws IOException, CancelledException {
+        try {
+            byte[] data = RplConverter.convertRpl(settings.provider(), (msg) -> settings.log().appendMsg(msg));
+            RplHeader rpl = new RplHeader(new ByteArrayProvider(data), (msg) -> settings.log().appendMsg(msg));
+            ElfProgramBuilder.loadElf(rpl, program, settings.options(), settings.log(), settings.monitor());
+        } catch (ElfException | DataFormatException var8) {
+            throw new IOException(var8.getMessage());
+        }
+    }
+
+
+    public void load(ByteProvider provider, LoadSpec loadSpec, List<Option> options, Program program,
+                     TaskMonitor monitor, MessageLog log) throws IOException, CancelledException {
+
+    }
 }
